83aba88f4bb330e1b9dfb6d26a23447a5ce4888d
Patryk Ostrowski <patrykost1998@gmail.com>
2021-03-23 19:44:26 +0100

    Johnson works but idk why returns NOne

--- ZAD1/Johnson.py
+++ ZAD1/Johnson.py
@@ -4,46 +4,55 @@ def two_machines_johnson(times:  list):
     tasks = [i for i in range(1, len(times[0])+1)]
     machine1 = [int(i) for i in times[0]]
     machine2 = [int(i) for i in times[1]]
-    on_begining = False
     list1, list2 = [], []
+
     while not tasks == []:
         merged = [*machine1, *machine2]
 
         min_element = min(merged)
         if min_element in machine1:
             index = machine1.index(min_element)
+            list1.append(index + 1)
         else:
             index = machine2.index(min_element)
+            list2.append(index + 1)
 
-        if on_begining:
-            list1.append(index+1)
-            on_begining = False
-
-        else:
-            list2.append(index+1)
-            on_begining = True
-
-        print(list1, list2)
         machine1[index] = inf
         machine2[index] = inf
         tasks.remove(index+1)
 
-    schedule = list1 + list(list2.__reversed__())
+    list2 = list(reversed(list2))
+    schedule = []
+    schedule.extend(list1)
+    schedule.extend(list2)
+    print(schedule)
     return schedule
 
-#Naprawic przydzielanie taskow do list
+
+
 
 def multi_machines_Johnson(times):
-    machine1 = [0 for _ in range(1, len(times[0])+1)]
-    machine2 = machine1
 
-    print(times)
-    half = int((len(times)) // 2)
+    if len(times) % 2  == 0:
+        half = len(times) // 2
+        list1 = times[:half]
+        list2 = times[half:]
+        list1 = [sum(i) for i in zip(*list1)]
+        list2 = [sum(i) for i in zip(*list2)]
 
-    for n in range(0, len(times[0])):
-        for i in range(0, half):
-            machine1[i] += times[n][i]
-        for j in range(half-1, len(times)):
-            machine2[j] += times[n][j]
+    else:
+        half = len(times) // 2
+        half_element = times[half]
+        list1 = times[0:half]
+        list2 = times[half+1:]
 
-    two_machines_johnson([*machine1, *machine2])
+        list1 = [sum(i) for i in zip(*list1)]
+        list2 = [sum(i) for i in zip(*list2)]
+
+        list1 = [a + b for a, b in zip(list1, half_element)]
+        list2 = [a + b for a, b in zip(list2, half_element)]
+
+    splited_lists = []
+    splited_lists.append(list1)
+    splited_lists.append(list2)
+    two_machines_johnson(splited_lists)
