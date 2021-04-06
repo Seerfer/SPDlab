77933ee51bd1f378d252c406275c6f203cd02b58
Patryk Ostrowski <patrykost1998@gmail.com>
2021-04-06 13:21:35 +0200

    It works but code is rly ugly

--- ZAD2/NEH.py
+++ ZAD2/NEH.py
@@ -1,19 +1,47 @@
-def count_priority(times):
-    tasks= list((map(list, zip(*times))))
+import math
+import Cmax
+
+def all_posibilities(lista, el):
+    posibilities = []
+    i = 0
+    for _ in range(len(lista)+1):
+        tmp = lista[:]
+        tmp.insert(i, el)
+        i = i+1
+        posibilities.append(tmp)
+    return posibilities
+
+def neh_cmax(tasks, schedule):
+    tmp_schedule = list(range(1, len(schedule)+1))
+    tmp_tasks =  []
+    for i in schedule:
+        tmp_tasks.append(tasks[i-1])
+    return Cmax.count_cmax(tmp_schedule, list((map(list, zip(*tmp_tasks)))))
+
+def neh(times):
+    tasks = list((map(list, zip(*times))))
     priorities_dict = {}
     index = 1
+
     for i in tasks:
         priorities_dict[index] = sum(i)
         index += 1
     priorities_dict = dict(sorted(priorities_dict.items(), key=lambda item: item[1], reverse=True))
-    result = []
 
-    max = True
+    result = []
     while list(priorities_dict.values()):
-        print(priorities_dict)
-        if max:
-            choose = list(priorities_dict.keys())[0]
-        else:
-            choose = list(priorities_dict.keys())[-1]
+        choose = list(priorities_dict.keys())[0]
         priorities_dict.pop(choose)
-        max = not max
+        if len(result) == 0:
+            result.append(choose)
+
+        else:
+            posibilities = all_posibilities(result, choose)
+            minimum = math.inf
+            for i in posibilities:
+                c_max = neh_cmax(tasks, i)
+                if c_max < minimum:
+                    minimum = c_max
+                    result = i
+
+    return result, minimum
