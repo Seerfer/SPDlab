3f070bb0bb9b89d5837327fe693a5be0e4fc1703
Patryk Ostrowski <patrykost1998@gmail.com>
2021-04-16 01:49:52 +0200

    Insert and swap done for neighbours

--- ZAD3/TS.py
+++ ZAD3/TS.py
@@ -1,12 +1,104 @@
-from neh import NEHmodifications
+import math
+from neh import NEHmodifications, Cmax
 from Johnson import Johnson
+import random
+from time import perf_counter
+def initialize_shedule(times, method="neh"):
+    if method == "neh":
+        if len(times) < 101:
+            return NEHmodifications.neh_ext4(times)[0]
+        else:
+            return Johnson.multi_machines_Johnson(times)
+    if method == "random":
+        return random.shuffle(list(range(1, len(times)+1)))
+    if method == "sequence":
+        return list(range(1, len(times)+1))
 
-def initialize_shedule(times):
-    if len(times) < 101:
-        return NEHmodifications.neh_ext4(times)[0]
+def neighbours(schedule, neighbours_number="all", function="swap", inverse_numbers=None):
+    neighbours = []
+    functions = function.split(",")
+    if str(neighbours_number) == "all":
+        if "swap" in functions:
+            for i in range(0, len(schedule)):
+                temp_schedule = schedule[:]
+                for j in range(i + 1, len(schedule)):
+                    temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
+                    neighbours.append(temp_schedule[:])
+                    temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
+
+            return neighbours
+        if "insert" in functions:
+            for i in range(0, len(schedule)):
+                temp_schedule = schedule[:]
+                current = temp_schedule.pop(i)
+                for j in range(0, len(schedule)):
+                    if j != i:
+                        temp_schedule.insert(j, current)
+                        if temp_schedule not in neighbours:
+                            neighbours.append(temp_schedule[:])
+                        temp_schedule.pop(j)
+
+            return neighbours
+        if "inverse" in functions:
+            pass
+
     else:
-        return Johnson.multi_machines_Johnson(times)
+        counter = 0
+        if "swap" in functions:
+            for i in range(0, int(neighbours_number)):
+                temp_schedule = schedule[:]
+                for j in range(i + 1, len(schedule)):
+                    temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
+                    neighbours.append(temp_schedule[:])
+                    temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
 
-def TS(times):
+            return neighbours
+        if "insert" in functions:
+            for i in range(0, len(schedule)):
+                temp_schedule = schedule[:]
+                current = temp_schedule.pop(i)
+                for j in range(0, len(schedule)):
+                    if j != i:
+                        temp_schedule.insert(j, current)
+                        if temp_schedule not in neighbours:
+                            neighbours.append(temp_schedule[:])
+                        temp_schedule.pop(j)
+
+            return neighbours
+        if "inverse" in functions:
+            pass
+
+def best_neighbour(schedules, times):
+    cmin = math.inf
+    tmp = None
+    for schedule in schedules:
+        cmax = Cmax.count_cmax(schedule,times)
+        if cmax < cmin:
+            cmin = cmax
+            tmp = schedule
+    return tmp
+
+def Tabu_search(times, naighbours, stop="iter", iter=5000, stop_time=1000000):
     schedule = initialize_shedule(times)
+    best = schedule
+    tabu = []
+    tabu.append(schedule)
 
+    if stop == "iter":
+        for i in range(0, iter):
+            neighbourhoods = neighbours(times, schedule)
+            best_cmax = math.inf
+            best_current = neighbours[0]
+            for i in range(1, len(neighbourhoods)):
+                pass
+
+
+
+    if stop == "times":
+        start = perf_counter()
+        end = math.inf
+        while end-start > stop_time:
+
+
+
+            end = perf_counter()
--- ZAD3/__main__.py
+++ ZAD3/__main__.py
@@ -1,5 +1,8 @@
 import Load
 from neh import NEHmodifications
 from neh import NEH
+import TS
+
 times = Load.readData("Data/tmp.txt")
 print(NEH.neh(times)[0])
+print(TS.neighbours(NEH.neh(times)[0], function="insert"))
