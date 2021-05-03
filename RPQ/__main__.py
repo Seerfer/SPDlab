4fcab5dc0b707d27196e6a2aa77397cd6e997d9c
Soksak <soksakf8@gmail.com>
2021-05-03 16:03:52 +0200

    Schrage_PMTN

--- RPQ/__main__.py
+++ RPQ/__main__.py
@@ -1,7 +1,9 @@
 import Load
 import Cmax
 import schrage
+import schrage_pmtn
 tasks = Load.readData("test.txt")
 
 
 print(schrage.schrage(tasks))
+print(schrage_pmtn.schrage_pmtn(tasks))
--- RPQ/schrage_pmtn.py
+++ RPQ/schrage_pmtn.py
@@ -1,24 +1,30 @@
 from Task import Task
 from typing import List
+import sys
 
-def schrage(tasks: List[Task]) -> List[Task]:
-    order = []
+def schrage_pmtn(tasks: List[Task]) -> List[Task]:
     ready = set()
     Cmax = 0
     not_ready = set(tasks)
+    l = Task(0, 0, sys.maxsize, 0)
     t = min(task.get_r() for task in not_ready)
     while ready or not_ready:
         while not_ready and min(task.get_r() for task in not_ready) <= t:
             e = min(not_ready, key=Task.get_r)
             ready.add(e)
             not_ready.remove(e)
+            if e.get_q() > l.get_q():
+                l.p = t - e.r
+                t = e.r
+                if l.p > 0:
+                    ready.add(l)
 
         if not ready:
             t = min(task.get_r() for task in not_ready)
         else:
             e = max(ready, key=Task.get_q)
             ready.remove(e)
-            order.append(e.id)
-            t += e.get_p()
-            Cmax = max(Cmax, t+e.q)
-    return order,Cmax
+            l = e
+            t += e.p
+            Cmax=max(Cmax,t+e.q)
+    return Cmax
