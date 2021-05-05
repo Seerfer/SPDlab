2457df1fb464516742817468084525f2b501979a
Patryk Ostrowski <patrykost1998@gmail.com>
2021-05-05 02:44:57 +0200

    Sprawozdanie, dodatkowe pliki danych

--- RPQ/__main__.py
+++ RPQ/__main__.py
@@ -1,12 +1,8 @@
 import Load
-import Cmax
 import schrage
-import schrage_pmtn
-from Heapq import Heapq
-import schrage_pmtn
 
-tasks = Load.readData("test.txt")
+tasks = Load.readData("Data/in200.txt")
 print(schrage.schrage(tasks))
 print(schrage.schrage_heapq(tasks))
-print(schrage_pmtn.schrage_pmtn(tasks))
-print(schrage_pmtn.schrage_pmtn_heapq(tasks))
+print(schrage.schrage_pmtn(tasks))
+print(schrage.schrage_pmtn_heapq(tasks))
